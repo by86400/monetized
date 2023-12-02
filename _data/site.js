55075575304d656e5be3a05547bca8e35eef8636
by86400 <mr86400@protonmail.com>
2023-12-02 08:10:27 -0700

    Changing config

--- _data/site.js
+++ _data/site.js
@@ -1,20 +1,20 @@
 module.exports = {
   meta: {
-    title: "My Purple Blog",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    title: "Distractions, Monetized",
+    description: "The project blog of Mr86400.",
     lang: "en",
-    siteUrl: "https://example.com/",
+    siteUrl: "https://monerized.by86400.today/",
   },
   feed: { // used in feed.xml.njk
     subtitle: "Lorem ipsum dolor sit amet consecuteor",
     filename: "atom.xml",
     path: "/atom.xml",
-    id: "https://example.com/",
-    authorName: "John Doe",
-    authorEmail: "johndoe@example.com"
+    id: "https://monetized.by86400.today",
+    authorName: "Mr. 86400",
+    authorEmail: "mr86400@protonmaill.com"
   },
   hero: { // used in hero section of main page ie. index.html.njk
-    title: "Welcome to my purple blog",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusantium deserunt odio esse."
+    title: "A Quest to Find a Financially Neutral Hobby",
+    description: "A project log that will have a lot of web stuff, but probably also 3d printing and knitting. Maybe some drawing. A little writing?"
   }
 }
